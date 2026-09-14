e7440dd304943c8042566aff41f86f7282067959
ziqryadam86-collab <ziqry.adam86@gmail.com>
2026-09-14 20:06:33 +0800

    Refactor ffmpeg command in start.sh
    Removed redundant decryption key option from ffmpeg command.

--- start.sh
+++ start.sh
@@ -18,12 +18,12 @@ do
   echo "Memulakan penstriman 8TV (Astro Source)..."
   
   ffmpeg \
+  -decryption_key 1a05bebf706408431a390c3f9f40f410:89c5ff9f8e65c7fe966afbd2f9128e5f \
   -use_timeline 1 \
   -live_start_index -3 \
   -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 \
   -err_detect ignore_err \
   -headers "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"$'\r\n'"Origin: https://sooka.my"$'\r\n'"Referer: https://sooka.my/"$'\r\n' \
-  -c:v decryption_key 1a05bebf706408431a390c3f9f40f410:89c5ff9f8e65c7fe966afbd2f9128e5f \
   -i "https://linearjitp-playback.astro.com.my/dash-wv/linear/509/default_ott.mpd" \
   -map 0:v:0 \
   -map 0:a:0 \
